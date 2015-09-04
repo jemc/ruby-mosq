753535551ce4d8b6ca840295d767ae11963a1141
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2015-09-04 13:42:43 -0700

    Don't call mosquitto_lib_cleanup from at_exit, because it causes issues with openssl is used from another at_exit handler, and mosquitto_lib_cleanup only cleans up SSL and ares libraries anyway. There is no safe way to do this in Ruby if another FFI or C ext happens to be using any of the libraries we clean from an ar_exit handler, so we just don't clean up after ourselves in this case..
    ¯\_(ツ)_/¯

--- lib/mosq.rb
+++ lib/mosq.rb
@@ -8,6 +8,3 @@ require_relative 'mosq/client'
 # Call to initialize the library
 Mosq::Util.error_check "initializing the libmosquitto library",
   Mosq::FFI.mosquitto_lib_init
-
-# Call cleanup at exit clean up the library
-at_exit { Mosq::FFI.mosquitto_lib_cleanup }
